ebe06cccb23c37ffc6e5cf84e296b87b683e9005
Myrmex <dfusi@hotmail.com>
2024-04-29 13:03:10 +0200

    OSD demo

--- src/app/img/sd-img-annotator-pg/sd-img-annotator-pg.component.ts
+++ src/app/img/sd-img-annotator-pg/sd-img-annotator-pg.component.ts
@@ -1,13 +1,29 @@
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import {
+  MAT_DIALOG_DEFAULT_OPTIONS,
+  MatDialog,
+  MatDialogConfig,
+} from '@angular/material/dialog';
 
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 
-import { AnnotationEvent } from '../../../../projects/myrmidon/cadmus-img-annotator/src/public-api';
+import {
+  Annotation,
+  AnnotationEvent,
+  AnnotoriousConfig,
+  GalleryImage,
+  ImgAnnotationList,
+  ImgAnnotatorToolbarComponent,
+} from '../../../../projects/myrmidon/cadmus-img-annotator/src/public-api';
 import { SdImgAnnotatorDirective } from '../../../../projects/myrmidon/cadmus-sdimg-annotator/src/public-api';
+import { MyImgAnnotationListComponent } from '../img-annotation-list/my-img-annotation-list.component';
+import { EditAnnotationDialogComponent } from '../edit-annotation-dialog/edit-annotation-dialog.component';
 
 @Component({
   standalone: true,
@@ -22,23 +38,92 @@ import { SdImgAnnotatorDirective } from '../../../../projects/myrmidon/cadmus-sd
     MatButtonToggleModule,
     MatIconModule,
     SdImgAnnotatorDirective,
+    ImgAnnotatorToolbarComponent,
+    MyImgAnnotationListComponent,
   ],
 })
 export class SdImgAnnotatorPgComponent {
-  public message?: string;
-  constructor() {}
+  private _sub?: Subscription;
+  // the list core used by the annotations list child component
+  private _list?: ImgAnnotationList<any>;
 
-  ngOnInit(): void {}
+  // the Annotorious annotator instance derived from the annotator directive
+  public annotator?: any;
+
+  // the annotation editor component type, used by the annotations list child
+  // component to create a new editor instance inside a popup dialog
+  public editor: any;
+
+  // the configuration provided to the annotator directive
+  public config?: AnnotoriousConfig = {
+    disableEditor: true,
+  };
+
+  public tool: string;
+  public image: GalleryImage;
+
+  public message?: string;
+  constructor(
+    public dialog: MatDialog,
+    @Inject(MAT_DIALOG_DEFAULT_OPTIONS) public dlgConfig: MatDialogConfig
+  ) {
+    this.editor = EditAnnotationDialogComponent;
+    this.tool = 'rect';
+    this.image = {
+      id: '1',
+      uri: 'assets/sample.jpg',
+      title: 'sample',
+      description: 'Sample image',
+    };
+  }
+
+  public onToolChange(tool: string): void {
+    this.tool = tool;
+  }
+
+  public onListInit(list: ImgAnnotationList<any>) {
+    this._list = list;
+  }
+
+  public onAnnotatorInit(annotator: any) {
+    setTimeout(() => {
+      this.annotator = annotator;
+    });
+  }
+
+  public onCreateSelection(annotation: Annotation) {
+    this._list?.onCreateSelection(annotation);
+  }
+
+  public onSelectAnnotation(annotation: Annotation) {
+    this._list?.onSelectAnnotation(annotation);
+  }
+
+  public onCancelSelected(annotation: Annotation) {
+    this._list?.onCancelSelected(annotation);
+  }
+
+  public editAnnotation(index: number): void {
+    this._list?.editAnnotation(index);
+  }
+
+  public selectAnnotation(index: number): void {
+    this._list?.selectAnnotation(index);
+  }
+
+  public removeAnnotation(index: number): void {
+    this._list?.removeAnnotation(index);
+  }
 
   public onCreateAnnotation(event: AnnotationEvent) {
-    this.message = 'CREATED:\n' + JSON.stringify(event.annotation, null, 2);
+    this._list?.onCreateAnnotation(event);
   }
 
   public onUpdateAnnotation(event: AnnotationEvent) {
-    this.message = 'UPDATED:\n' + JSON.stringify(event.annotation, null, 2);
+    this._list?.onUpdateAnnotation(event);
   }
 
   public onDeleteAnnotation(event: AnnotationEvent) {
-    this.message = 'DELETED:\n' + JSON.stringify(event.annotation, null, 2);
+    this._list?.onDeleteAnnotation(event);
   }
 }
