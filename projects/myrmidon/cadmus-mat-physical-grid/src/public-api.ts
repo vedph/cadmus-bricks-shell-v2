0bb6c6241f3f6ef15ac73b1d9eb38c33274ebb79
Myrmex <dfusi@hotmail.com>
2024-07-15 12:02:33 +0200

    Grid service

--- projects/myrmidon/cadmus-mat-physical-grid/src/public-api.ts
+++ projects/myrmidon/cadmus-mat-physical-grid/src/public-api.ts
@@ -4,3 +4,4 @@
 
 export * from './lib/components/physical-grid-location/physical-grid-location.component';
 export * from './lib/pipes/excel-column.pipe';
+export * from './lib/services/physical-grid-coords.service';
